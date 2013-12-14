6303be153887fd04ab4125c54b6e30a490cc96b6
Piotr Foltyn <piotr.foltyn@gmail.com>
2013-12-14 01:43:45 +0000

    Shaders in separate files.

--- infragram.js
+++ infragram.js
@@ -72,6 +72,7 @@ drawScene = function(ctx, returnImage) {
 };
 
 generateShader = function(ctx, r, g, b) {
+  var code;
   r = r.toLowerCase().replace(/h/g, "r").replace(/s/g, "g").replace(/v/g, "b");
   g = g.toLowerCase().replace(/h/g, "r").replace(/s/g, "g").replace(/v/g, "b");
   b = b.toLowerCase().replace(/h/g, "r").replace(/s/g, "g").replace(/v/g, "b");
@@ -90,7 +91,11 @@ generateShader = function(ctx, r, g, b) {
   if (b === "") {
     b = "b";
   }
-  document.getElementById("shader-fs").text = "        precision mediump float;        varying vec2 vTextureCoord;        uniform sampler2D uSampler;        uniform float uSlider;        uniform float uNdvi;        uniform float uGreyscale;        uniform float uHsv;        uniform float uColormap;        vec4 greyscale_colormap(float n)        {            vec3 x  = vec3(0.0, 1.0, 0.0);            vec3 y0 = vec3(0.0, 0.0, 0.0);            vec3 y1 = vec3(255.0, 255.0, 255.0) / 255.0;            return vec4(                (n - x[0]) / (x[1] - x[0]) * (y1[0] - y0[0]) + y0[0],                (n - x[0]) / (x[1] - x[0]) * (y1[1] - y0[1]) + y0[1],                (n - x[0]) / (x[1] - x[0]) * (y1[2] - y0[2]) + y0[2],                1.0);        }        vec4 color_colormap(float n)        {            vec3 x = vec3(0.0, 0.5, 0.0);            vec3 y0 = vec3(25.0, 0.0, 175.0) / 255.0;            vec3 y1 = vec3(38.0, 195.0, 195.0) / 255.0;            if (n >= 0.5)            {                x = vec3(0.5, 0.75, 0.0);                y0 = vec3(50.0, 155.0, 60.0) / 255.0;                y1 = vec3(195.0, 190.0, 90.0) / 255.0;            }            else if (n >= 0.75)            {                x = vec3(0.75, 1.0, 0.0);                y0 = vec3(195.0, 190.0, 90.0) / 255.0;                y1 = vec3(185.0, 50.0, 50.0) / 255.0;            }            return vec4(                (n - x[0]) / (x[1] - x[0]) * (y1[0] - y0[0]) + y0[0],                (n - x[0]) / (x[1] - x[0]) * (y1[1] - y0[1]) + y0[1],                (n - x[0]) / (x[1] - x[0]) * (y1[2] - y0[2]) + y0[2],                1.0);        }        vec4 rgb2hsv(vec4 c)        {            vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);            vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));            vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));            float d = q.x - min(q.w, q.y);            float e = 1.0e-10;            return vec4(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x, 1.0);        }        vec4 hsv2rgb(vec4 c)        {            vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);            vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);            return vec4(c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y), 1.0);        }        void main(void)        {            vec4 color = texture2D(uSampler, vTextureCoord);            if (uColormap >= 1.0)            {                color = vec4(vTextureCoord, 0.0, 0.0);            }            else if (uHsv >= 1.0)            {                color = rgb2hsv(color);            }            float x = uSlider;            float r = color.r;            float g = color.g;            float b = color.b;" + "float rr = " + r + ";" + "float gg = " + g + ";" + "float bb = " + b + ";" + "if (uNdvi < 1.0)            {                color = vec4(rr, gg, bb, 1.0);                gl_FragColor = (uHsv < 1.0) ? color : hsv2rgb(color);            }            else            {                gl_FragColor = (uGreyscale < 1.0) ? color_colormap(rr) : greyscale_colormap(rr);            }        }";
+  code = $("#shader-fs-template").html();
+  code = code.replace(/@1@/g, r);
+  code = code.replace(/@2@/g, g);
+  code = code.replace(/@3@/g, b);
+  $("#shader-fs").html(code);
   return initShaders(ctx);
 };
 
@@ -153,9 +158,14 @@ initTexture = function(ctx, fileObject) {
   return ctx.image.src = fileObject;
 };
 
-$("document").ready(function() {
+$(window).load(function() {
+  $("#shader-vs").load("shader.vert");
+  $("#shader-fs-template").load("shader.frag");
   img_context = createContext("canvas-image");
-  map_context = createContext("colorbar");
+  return map_context = createContext("colorbar");
+});
+
+$(document).ready(function() {
   $("#file-sel").change(function() {
     var reader;
     if (this.files && this.files[0]) {
@@ -184,17 +194,17 @@ $("document").ready(function() {
   $('#download').click(function() {
     return download(img_context);
   });
-  $('#infragrammar_hsv').submit(function(event) {
+  $('#infragrammar_hsv').submit(function() {
     setMode("hsv");
     generateShader(img_context, $('#h_exp').val(), $('#s_exp').val(), $('#v_exp').val());
     return drawScene(img_context);
   });
-  $('#infragrammar').submit(function(event) {
+  $('#infragrammar').submit(function() {
     setMode("rgb");
     generateShader(img_context, $('#r_exp').val(), $('#g_exp').val(), $('#b_exp').val());
     return drawScene(img_context);
   });
-  $('#infragrammar_mono').submit(function(event) {
+  $('#infragrammar_mono').submit(function() {
     setMode("mono");
     generateShader(img_context, $('#m_exp').val(), $('#m_exp').val(), $('#m_exp').val());
     return drawScene(img_context);
